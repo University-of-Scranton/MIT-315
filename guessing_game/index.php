3d2edc1ca4f1faebed15f7bbf6f1a92cb73e198b
Joe Casabona <jcasabona@gmail.com>
2014-09-17 08:26:50 -0400

    Working Guessing Game
    Sessions, ifs, arrays, oh my!

--- guessing_game/index.php
+++ guessing_game/index.php
@@ -4,30 +4,24 @@ Instructions: Create a Number Guessing game.
 Assign a number to a variable and ask the user to enter a guess.
 If the Guess is correct, say so. 
 If it is not, say it is not and then print out the correct answer
-
-**Make this a HW assignment. Do something different in class
-to demonstrate input and if statements***
-
-
-Next steps for assignment:
-
-Randomly generate number. 
-Use buttons to ask the user to play with a new number
-Save wrong guesses in array. 
-Make guess check a function
-
-
 ********/
 
+session_start();
 
 function guess_check($guess, $answer){
-	if($guess > $answer){
+	$right= false;
+	if(in_array($guess, $_SESSION['guesses'])){
+		print "<h3>You already guessed $guess!</h3>";
+	}else if($guess > $answer){
 		print "<h3>$guess was a too high. Try again!</h3>";
 	}else if($guess < $answer){
 		print "<h3>$guess was too low. Try again!</h3>";
 	}else{
 		print "<h3>Nice work! $guess is correct!</h3>";
+		$right= true;
 	}
+
+	return $right;
 }
 
 
@@ -45,30 +39,36 @@ function guess_check($guess, $answer){
 
 <?php
 
-if(!isset($_POST['number'])){
-	$number= rand(1,20);
-}else{
-	$number= $_POST['number'];
+if(isset($_POST['new_game'])){
+	session_destroy();
+}
+
+if(!isset($_SESSION['number'])){
+	$_SESSION['number']= rand(1,20);
 }
 
 
 if(isset($_POST['guess'])){
-
-	guess_check($_POST['guess'], $number);
+	$right= guess_check($_POST['guess'], $_SESSION['number']);
+	$_SESSION['guesses'][]= $_POST['guess'];
 }
 
 
 ?>
 
+<?php if($right){ ?>
+<form method="post">
+	<p><input type="submit" name="new_game" value="Play Again?" /></p>
+</form>
+<?php } else { ?>
 
 <p>Please guess a number between 1 and 20</p>
-
 <form method="post">
 		<p><input type="text" placeholder="Guess?" name="guess" /></p>
-		<input type="hidden" name="number" value="<?php print $number; ?>" />
 		<p><input type="submit" name="submit" value="submit" /></p>
 </form>
 
+<?php } ?>
 
 </body>
 
